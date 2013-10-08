9dabc3d18c7c198e035a04ca64c3a371576c2d04
Hugo Torres <hugo.torres1993@gmail.com>
2013-10-07 22:12:03 -0400

    Moved C++ to its own folder

---
+++ DataStructures/C++_implementation/sources/testing.cpp
@@ -0,0 +1,25 @@
+#include "../headers/LinkedList.h"
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+    LinkedList<int> myList;
+
+    myList.insert(4);
+    printf("Current element: %d\n", myList.getCurrentElement());
+
+    myList.insert(5);
+    //printf("Current element: %d\n", myList.getCurrentElement());
+
+    myList.append(6);
+    //myList.jumpToTail();
+    //printf("Current element: %d\n", myList.getCurrentElement());
+
+    myList.jumpToHead();
+    for(int i = 0; i < myList.getCurrentSize(); i++){
+        printf("Current element: %d\n", myList.getCurrentElement());
+        myList.nextNode();
+    }
+
+	return 0;
+}
