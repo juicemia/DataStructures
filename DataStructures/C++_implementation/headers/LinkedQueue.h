7796d50ac0a6987860845429513e46be58328982
Hugo Torres <hugo.torres1993@gmail.com>
2013-10-13 00:54:10 -0400

    Finished implementing enqueue and dequeue

--- DataStructures/C++_implementation/headers/LinkedQueue.h
+++ DataStructures/C++_implementation/headers/LinkedQueue.h
@@ -4,7 +4,6 @@ template <typename T>
 class LinkedQueue
 {
     private:
-        SingleNode<T>* head;
         SingleNode<T>* tail;
         int currentSize;
 
@@ -23,24 +22,21 @@ class LinkedQueue
 template <typename T>
 LinkedQueue<T>::LinkedQueue()
 {
-    head = tail = NULL;
+    tail = NULL;
     currentSize = 0;
 }
 
 template <typename T>
 LinkedQueue<T>::LinkedQueue(T elem_init)
 {
-    head = tail = new SingleNode<T>(elem_init, NULL);
+    tail = new SingleNode<T>(elem_init, NULL);
     currentSize = 1;
 }
 
 template <typename T>
 LinkedQueue<T>::~LinkedQueue()
 {
-    if(currentSize > 1)
-        delete [] head;
-    else if(currentSize == 1)
-        delete head;
+
 }
 
 template <typename T>
@@ -52,5 +48,37 @@ int LinkedQueue<T>::getCurrentSize()
 template <typename T>
 T LinkedQueue<T>::getCurrentElement()
 {
-    return head->element;
+    SingleNode<T>* tmp = tail;
+    while(tmp->next != NULL)
+        tmp = tmp->next;
+
+    return tmp->element;
+}
+
+template <typename T>
+int LinkedQueue<T>::enqueue(T elem)
+{
+    currentSize++;
+
+    if(currentSize == 1){
+        tail = new SingleNode<T>(elem, NULL);
+        return currentSize;
+    }
+
+    tail = new SingleNode<T>(elem, tail);
+    return currentSize;
+}
+
+template <typename T>
+T LinkedQueue<T>::dequeue(){
+    currentSize--;
+    SingleNode<T>* tmp = tail;
+    while(tmp->next->next != NULL)
+        tmp = tmp->next;
+
+    T ret = tmp->next->element;
+    delete tmp->next;
+    tmp->next = NULL;
+
+    return ret;
 }
--- DataStructures/C++_implementation/sources/testing.cpp
+++ DataStructures/C++_implementation/sources/testing.cpp
@@ -9,5 +9,15 @@ int main(int argc, char* argv[])
     printf("Queue current size: %d\n", myQueue.getCurrentSize());
     printf("Queue current element: %d\n", myQueue.getCurrentElement());
 
+    myQueue.enqueue(5);
+
+    printf("Queue current size: %d\n", myQueue.getCurrentSize());
+    printf("Queue current element: %d\n", myQueue.getCurrentElement());
+
+    a = myQueue.dequeue();
+    printf("Queue current size: %d\n", myQueue.getCurrentSize());
+    printf("Queue dequeued element: %d\n", a);
+    printf("Queue current element: %d\n", myQueue.getCurrentElement());
+
 	return 0;
 }
