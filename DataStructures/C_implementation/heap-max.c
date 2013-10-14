2257ba2e6c132fdc3b74b4a2b1108497bc033edb
Lukas Bijaminas <lukas_dmx@yahoo.com>
2013-10-14 12:49:38 -0400

    Updated heap

--- DataStructures/C_implementation/heap-max.c
+++ DataStructures/C_implementation/heap-max.c
@@ -1,7 +1,6 @@
 #include "heap.h"
 
 static void heap2Tree(tree_node_t**, int);
-static void removeTreeValue(tree_node_t **);
 static void copyNode(tree_node_t *, tree_node_t **);
 static void printNode(tree_node_t *);
 static void removeNode(bst_t *, tree_node_t *, int, tree_node_t **);
@@ -135,17 +134,6 @@ static void removeNode(bst_t *tree, tree_node_t *node, int value, tree_node_t **
 	
 }
 
-static void removeTreeValue(tree_node_t **node){
-	/*
-	 * Recursive method to remove each tree node one
-	 * by one
-	 */
-	if (*node == NULL) return;
-	//post-order traversal
-	removeTreeValue(&(*node)->left);
-	removeTreeValue(&(*node)->right);
-	free(*node);
-}
 static void add2Tree(tree_node_t** node, int value){
 	/*
 	 * A recursive method to add a @param value to 
