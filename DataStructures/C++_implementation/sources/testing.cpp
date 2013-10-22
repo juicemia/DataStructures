67a5357140f8042908c95a20d5f4dddecf67fa48
Hugo Torres <hugo.torres1993@gmail.com>
2013-10-22 18:40:07 -0400

    Played with it some more

--- DataStructures/C++_implementation/sources/testing.cpp
+++ DataStructures/C++_implementation/sources/testing.cpp
@@ -4,12 +4,13 @@
 int main(int argc, char* argv[])
 {
     BinaryTree<int> myTree(3);
-    //myTree.head = new BTreeNode<int>(4);
-
     printf("Tree head: %d\n", myTree.head->getElement());
 
     myTree.head->setLeftChild(new BTreeNode<int>(4));
-    printf("Head->left: %d", myTree.head->getLeftChild()->getElement());
+    printf("Head->left: %d\n", myTree.head->getLeftChild()->getElement());
+
+    myTree.head->setRightChild(new BTreeNode<int>(5));
+    printf("Head->right: %d", myTree.head->getRightChild()->getElement());
 
 	return 0;
 }
