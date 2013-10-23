5540323298e6ad7e7a52472bfc629e8e93bf0e70
LeeGiS <lukas_dmx@yahoo.com>
2013-10-23 18:48:23 -0400

    Added some useful defines

--- DataStructures/C_implementation/doublyLinkedList.c
+++ DataStructures/C_implementation/doublyLinkedList.c
@@ -1,5 +1,11 @@
 #include "doublyLinkedList.h"
 
+#define NEXT node->next
+#define PREVIOUS node->prev
+#define NODE_VAL node->value
+#define FIRST list->first
+#define LAST list->last
+
 static void addFirst(doubly_linked_list_t*, dl_node_t*);
 
 /*
@@ -8,7 +14,7 @@ static void addFirst(doubly_linked_list_t*, dl_node_t*);
 	have any elements is pointless.
 */
 /*
-	`*prevNode` used in many functions signifies the previous node, whereas 
+	`*prevNode` used in many functions signifies the previous node, whereas
 	`*tempNode` signifies the node that will be removed, and it is needed
 	for the memory to be released by `free()`
 */
@@ -18,14 +24,14 @@ void addTop(doubly_linked_list_t* list, int value){
 	newNode->value = value;
 	newNode->next = NULL;
 	newNode->prev = NULL;
-	
+
 	if (list->count == 0){
 		// If no items exist in the list
 		addFirst(list, newNode);
 	} else {
-		newNode->next = list->first;
-		list->first->prev = newNode;
-		list->first = newNode;	
+		newNode->next = FIRST;
+		FIRST->prev = newNode;
+		FIRST = newNode;
 	}
 	list->count++;
 }
@@ -40,59 +46,59 @@ void addBottom(doubly_linked_list_t* list, int value){
 		// If no items exist in the list
 		addFirst(list, newNode);
 	} else {
-		list->last->next = newNode;
-		newNode->prev = list->last; // set the prev node
-		list->last = newNode;	
+		LAST->next = newNode;
+		newNode->prev = LAST; // set the prev node
+		LAST = newNode;
 	}
 	list->count++;
 }
 void removeTop(doubly_linked_list_t* list){
 	if (list->count != 0){
 		list->count--;
-		dl_node_t *tempNode = list->first;
-		list->first = tempNode->next;
-		if (list->count != 0) list->first->prev = NULL; // reset the previous node pointer, if it wasn't the last item to be removed
+		dl_node_t *tempNode = FIRST;
+		FIRST = tempNode->next;
+		if (list->count != 0) FIRST->prev = NULL; // reset the previous node pointer, if it wasn't the last item to be removed
 		free(tempNode); // free the removed memory
 	}
 }
 void removeBottom(doubly_linked_list_t* list){
-	if (list->count != 0){	
+	if (list->count != 0){
 		list->count--;
-		dl_node_t *node = list->first;
+		dl_node_t *node = FIRST;
 		dl_node_t *prevNode = node;
-		dl_node_t *tempNode;	
-		
+		dl_node_t *tempNode;
+
 		// loop reassigns all the nodes but the last one
-		while(node->next != NULL){
+		while(NEXT != NULL){
 			prevNode = node;
-			node = node->next;
+			node = NEXT;
 		}
 		tempNode = node; //the last node
 		free(tempNode); // free memory
 		if (list->count != 0){ // needed since the count is decremented at the very beginning
 			prevNode->next = NULL;
-			list->last = prevNode;
+			LAST = prevNode;
 		} else{
 			// Last element to be deleted
-			list->last = NULL;
-			list->first = NULL;
+			LAST = NULL;
+			FIRST = NULL;
 		}
 	}
 }
 void insertAfter(doubly_linked_list_t* list, int search /* Value to be searched for */, int newVal){
 	if (list->count != 0){
-		dl_node_t *node = list->first;
+		dl_node_t *node = FIRST;
 		dl_node_t *newNode = malloc(sizeof (dl_node_t));
 		newNode->value = newVal;
 		while(node != NULL){
-			if (node->value == search){
-				newNode->next = node->next;
+			if (NODE_VAL == search){
+				newNode->next = NEXT;
 				newNode->prev = node; // assign the previous node
-				node->next = newNode;
+				NEXT = newNode;
 				list->count++;
 				break;
 			}
-			node = node->next;
+			node = NEXT;
 		}
 	} else {
 		addTop(list, newVal);
@@ -100,26 +106,26 @@ void insertAfter(doubly_linked_list_t* list, int search /* Value to be searched 
 }
 void insertBefore(doubly_linked_list_t* list, int search /* Value to be searched for */, int newVal){
 	if (list->count != 0){
-		dl_node_t *node = list->first;
+		dl_node_t *node = FIRST;
 		dl_node_t *newNode = malloc(sizeof (dl_node_t));
 
 		newNode->value = newVal;
-		if (node->value == search){ // quick way to check the first value
+		if (NODE_VAL == search){ // quick way to check the first value
 			addTop(list, newVal);
 			return;
 		}
 
-		node = node->next; // since we checked the first one, let's move to the next one
+		node = NEXT; // since we checked the first one, let's move to the next one
 
 		while(node != NULL){
-			if (node->value == search){
+			if (NODE_VAL == search){
 				newNode->next = node;
-				newNode->prev = node->prev; //set the previous node
-				node->prev->next = newNode;
+				newNode->prev = PREVIOUS; //set the previous node
+				PREVIOUS->next = newNode;
 				list->count++;
 				break;
 			}
-			node = node->next;
+			node = NEXT;
 		}
 	} else {
 		addTop(list, newVal);
@@ -127,93 +133,93 @@ void insertBefore(doubly_linked_list_t* list, int search /* Value to be searched
 }
 void removeAfter(doubly_linked_list_t* list, int search /* Value to be searched for */){
 	if (list->count != 0){
-		dl_node_t *node = list->first;
+		dl_node_t *node = FIRST;
 		dl_node_t *tempNode;
 		while(node != NULL){
-			if (node->value == search){
-				tempNode = node->next;
+			if (NODE_VAL == search){
+				tempNode = NEXT;
 				if (tempNode != NULL) {
 					// The node to be removed exists
-					node->next = tempNode->next;
-					if (tempNode->next != NULL)	tempNode->next->prev = node; // if node->next is null it will not have a prev pointer
+					NEXT = tempNode->next;
+					if (tempNode->next != NULL)	tempNode->next->prev = node; // if NEXT is null it will not have a prev pointer
 					free(tempNode);
 					list->count--;
-				} 
+				}
 				// If the node to be removed doesn't exist, do nothing
 				break;
 			}
-			node = node->next;
+			node = NEXT;
 		}
 	}
 }
 
 void removeBefore(doubly_linked_list_t* list, int search /* Value to be searched for */){
 	if (list->count != 0){
-		dl_node_t *node = list->first;
+		dl_node_t *node = FIRST;
 		dl_node_t *tempNode;
 
-		if (node->value == search){ // quick way to check the first value
+		if (NODE_VAL == search){ // quick way to check the first value
 			// There is no value to be removed before the first one
 			return;
 		}
-		node = node->next; // since we checked the first one, let's move to the next one
-	
+		node = NEXT; // since we checked the first one, let's move to the next one
+
 		while(node != NULL){
-			if (node->value == search){
-				tempNode = node->prev;
+			if (NODE_VAL == search){
+				tempNode = PREVIOUS;
 				tempNode->prev->next = node; // said the node before the 'to be removed' node to point to the current node
-				node->prev = tempNode->prev;
+				PREVIOUS = tempNode->prev;
 				free(tempNode);
 				list->count--;
 				break;
 			}
-			node = node->next;
+			node = NEXT;
 		}
 	}
 }
 void removeNode(doubly_linked_list_t* list, int search /* Value to be searched for */){
 	if (list->count != 0){
-		dl_node_t *node = list->first;
-		if (node->value == search){ // checking the first value
+		dl_node_t *node = FIRST;
+		if (NODE_VAL == search){ // checking the first value
 			// All we have to do is just remove the top value
 			removeTop(list);
 			return;
 		}
-		node = node->next; // not the first one
-		
+		node = NEXT; // not the first one
+
 		while(node != NULL){
-			if (node->value == search){
-				node->prev->next = node->next; // reset prev node pointer to point to next node
-				if (node->next != NULL) node->next->prev = node->prev; // if node->next is null it will not have a prev pointer
+			if (NODE_VAL == search){
+				PREVIOUS->next = NEXT; // reset prev node pointer to point to next node
+				if (NEXT != NULL) NEXT->prev = PREVIOUS; // if NEXT is null it will not have a prev pointer
 				free(node);
 				list->count--;
 				break;
 			}
-			node = node->next;
+			node = NEXT;
 		}
 	}
 }
 void printList(doubly_linked_list_t* list){
-	dl_node_t *node = list->first;
+	dl_node_t *node = FIRST;
 	int val;
 	while(node != NULL){
-		val = node->value;
+		val = NODE_VAL;
 		printf("%d \n", val);
-		node = node->next;
+		node = NEXT;
 	}
 }
 
 static void addFirst(doubly_linked_list_t* list, dl_node_t* node){
 	// This is called on the occasion that the list is empty
-	list->first = node;
-	list->last = node;
+	FIRST = node;
+	LAST = node;
 }
 
 int contains(doubly_linked_list_t* list, int item){
-	dl_node_t *node = list->first;
+	dl_node_t *node = FIRST;
 	while(node != NULL){
-		if (node->value == item) return 1;
-		node = node->next;
+		if (NODE_VAL == item) return 1;
+		node = NEXT;
 	}
 	return 0;
 }
@@ -223,38 +229,38 @@ void copy(doubly_linked_list_t* list, doubly_linked_list_t* target){
 		// First let's copy the first item
 		dl_node_t *newNode = malloc(sizeof (dl_node_t)); // this will hold temporary nodes
 		dl_node_t *node;				   // this will hold nodes to the new linked list
-		dl_node_t *oldNode = list->first;		   // this will hold nodes from the old linked list
+		dl_node_t *oldNode = FIRST;		   // this will hold nodes from the old linked list
 		int i;					   // counter for loop
 		target->first = newNode;
 		node = target->first;
-		node->value = oldNode->value;
-		node->prev = NULL;
+		NODE_VAL = oldNode->value;
+		PREVIOUS = NULL;
 		while(oldNode->next != NULL){
 			oldNode = oldNode->next;	// advance the node pointer
 			newNode = malloc(sizeof (dl_node_t));	// allocate space for the new node
-			node->next = newNode;
+			NEXT = newNode;
 			newNode->prev = node;
-			
-			node = node->next;
-			node->value = oldNode->value;
+
+			node = NEXT;
+			NODE_VAL = oldNode->value;
 		}
-		
+
 		target->last = node;
 		target->last->next = NULL; // make sure that last doesn't point anywhere
-		
+
 		// copy the count, of course
 		target->count = list->count;
 	}
 }
 
 void emptyDLL(doubly_linked_list_t* list){
-	dl_node_t *node = list->first;
+	dl_node_t *node = FIRST;
 	while(node != NULL){
-		free(node->prev);
-		node = node->next;
+		free(PREVIOUS);
+		node = NEXT;
 	}
 	free(node); // free the last node
 	list->count = 0;
-	list->first = NULL;
-	list->last = NULL;
+	FIRST = NULL;
+	LAST = NULL;
 }
--- DataStructures/C_implementation/linkedList.c
+++ DataStructures/C_implementation/linkedList.c
@@ -1,5 +1,10 @@
 #include "linkedList.h"
 
+#define NEXT node->next
+#define NODE_VAL node->value
+#define FIRST list->first
+#define LAST list->last
+
 static void addFirst(linked_list_t*, node_t*);
 
 /*
@@ -8,7 +13,7 @@ static void addFirst(linked_list_t*, node_t*);
 	have any elements is pointless.
 */
 /*
-	`*prevNode` used in many functions signifies the previous node, whereas 
+	`*prevNode` used in many functions signifies the previous node, whereas
 	`*tempNode` signifies the node that will be removed, and it is needed
 	for the memory to be released by `free()`
 */
@@ -22,8 +27,8 @@ void addTop(linked_list_t* list, int value){
 		// If no items exist in the list
 		addFirst(list, newNode);
 	} else {
-		newNode->next = list->first;
-		list->first = newNode;	
+		newNode->next = FIRST;
+		FIRST = newNode;
 	}
 	list->count++;
 }
@@ -36,56 +41,56 @@ void addBottom(linked_list_t* list, int value){
 		// If no items exist in the list
 		addFirst(list, newNode);
 	} else {
-		list->last->next = newNode;
-		list->last = newNode;	
+		LAST->next = newNode;
+		LAST = newNode;
 	}
 	list->count++;
 }
 void removeTop(linked_list_t* list){
 	if (list->count != 0){
 		list->count--;
-		node_t *tempNode = list->first;
-		list->first = list->first->next;
+		node_t *tempNode = FIRST;
+		FIRST = FIRST->next;
 		free(tempNode); // free the removed memory
 	}
 }
 void removeBottom(linked_list_t* list){
-	if (list->count != 0){	
+	if (list->count != 0){
 		list->count--;
-		node_t *node = list->first;
+		node_t *node = FIRST;
 		node_t *prevNode = node;
-		node_t *tempNode;	
-		
+		node_t *tempNode;
+
 		// loop reassigns all the nodes but the last one
-		while(node->next != NULL){
+		while(NEXT != NULL){
 			prevNode = node;
-			node = node->next;
+			node = NEXT;
 		}
 		tempNode = node; //the last node
 		free(tempNode); // free memory
 		if (list->count != 0){ // needed since the count is decremented at the very beginning
 			prevNode->next = NULL;
-			list->last = prevNode;
+			LAST = prevNode;
 		} else{
 			// Last element to be deleted
-			list->last = NULL;
-			list->first = NULL;
+			LAST = NULL;
+			FIRST = NULL;
 		}
 	}
 }
 void insertAfter(linked_list_t* list, int search /* Value to be searched for */, int newVal){
 	if (list->count != 0){
-		node_t *node = list->first;
+		node_t *node = FIRST;
 		node_t *newNode = malloc(sizeof (node_t));
 		newNode->value = newVal;
 		while(node != NULL){
-			if (node->value == search){
-				newNode->next = node->next;
-				node->next = newNode;
+			if (NODE_VAL == search){
+				newNode->next = NEXT;
+				NEXT = newNode;
 				list->count++;
 				break;
 			}
-			node = node->next;
+			node = NEXT;
 		}
 	} else {
 		addTop(list, newVal);
@@ -93,25 +98,25 @@ void insertAfter(linked_list_t* list, int search /* Value to be searched for */,
 }
 void insertBefore(linked_list_t* list, int search /* Value to be searched for */, int newVal){
 	if (list->count != 0){
-		node_t *node = list->first;
+		node_t *node = FIRST;
 		node_t *prevNode = node;
 		node_t *newNode = malloc(sizeof (node_t));
 		newNode->value = newVal;
-		if (node->value == search){ // quick way to check the first value
+		if (NODE_VAL == search){ // quick way to check the first value
 			addTop(list, newVal);
 			return;
 		}
-		node = node->next; // since we checked the first one, let's move to the next one
+		node = NEXT; // since we checked the first one, let's move to the next one
 
 		while(node != NULL){
-			if (node->value == search){
+			if (NODE_VAL == search){
 				newNode->next = node;
 				prevNode->next = newNode;
 				list->count++;
 				break;
 			}
 			prevNode = node;
-			node = node->next;
+			node = NEXT;
 		}
 	} else {
 		addTop(list, newVal);
@@ -119,105 +124,105 @@ void insertBefore(linked_list_t* list, int search /* Value to be searched for */
 }
 void removeAfter(linked_list_t* list, int search /* Value to be searched for */){
 	if (list->count != 0){
-		node_t *node = list->first;
+		node_t *node = FIRST;
 		node_t *tempNode;
 		while(node != NULL){
-			if (node->value == search){
-				tempNode = node->next;
+			if (NODE_VAL == search){
+				tempNode = NEXT;
 				if (tempNode != NULL) {
 					// The node to be removed exists
-					node->next = tempNode->next;
+					NEXT = tempNode->next;
 					free(tempNode);
 					list->count--;
-				} 
+				}
 				// If the node to be removed doesn't exist, do nothing
 				break;
 			}
-			node = node->next;
+			node = NEXT;
 		}
 	}
 }
 
 void removeBefore(linked_list_t* list, int search /* Value to be searched for */){
 	if (list->count != 0){
-		node_t *node = list->first;
+		node_t *node = FIRST;
 		node_t *tempNode;
 		node_t *prevNode = node;
 		node_t *prevPrevNode = node; // the node before the previous node
 
-		if (node->value == search){ // quick way to check the first value
+		if (NODE_VAL == search){ // quick way to check the first value
 			// There is no value to be removed before the first one
 			return;
 		}
-		node = node->next; // since we checked the first one, let's move to the next one
-		if (node->value == search){ // checking the second value
+		node = NEXT; // since we checked the first one, let's move to the next one
+		if (NODE_VAL == search){ // checking the second value
 			// All we have to do is just remove the top value
 			removeTop(list);
 			return;
 		}
 		prevNode = node;
-		node = node->next; // not first two; move on
-		
+		node = NEXT; // not first two; move on
+
 		while(node != NULL){
-			if (node->value == search){
+			if (NODE_VAL == search){
 				tempNode = prevNode;
 				prevPrevNode->next = node; // said the node before the 'to be removed' node to point to the current node
 				free(tempNode);
 				list->count--;
 				break;
 			}
-			
+
 			prevPrevNode = prevNode;
 			prevNode = node;
-			node = node->next;
+			node = NEXT;
 		}
 	}
 }
 void removeNode(linked_list_t* list, int search /* Value to be searched for */){
 	if (list->count != 0){
-		node_t *node = list->first;
+		node_t *node = FIRST;
 		node_t *prevNode = node;
-		if (node->value == search){ // checking the first value
+		if (NODE_VAL == search){ // checking the first value
 			// All we have to do is just remove the top value
 			removeTop(list);
 			return;
 		}
-		node = node->next; // not the first one
-		
+		node = NEXT; // not the first one
+
 		while(node != NULL){
-			if (node->value == search){
-				prevNode->next = node->next; // reset prev node pointer to point to next node
+			if (NODE_VAL == search){
+				prevNode->next = NEXT; // reset prev node pointer to point to next node
 				free(node);
 				list->count--;
 				break;
 			}
-			
+
 			prevNode = node;
-			node = node->next;
+			node = NEXT;
 		}
 	}
 }
 void printList(linked_list_t* list){
-	node_t *node = list->first;
+	node_t *node = FIRST;
 	int val;
 	while(node != NULL){
-		val = node->value;
+		val = NODE_VAL;
 		printf("%d \n", val);
-		node = node->next;
+		node = NEXT;
 	}
 }
 
 static void addFirst(linked_list_t* list, node_t* node){
 	// This is called on the occasion that the list is empty
-	list->first = node;
-	list->last = node;
+	FIRST = node;
+	LAST = node;
 }
 
 int contains(linked_list_t* list, int item){
-	node_t *node = list->first;
+	node_t *node = FIRST;
 	while(node != NULL){
-		if (node->value == item) return 1;
-		node = node->next;
+		if (NODE_VAL == item) return 1;
+		node = NEXT;
 	}
 	return 0;
 }
@@ -227,36 +232,36 @@ void copy(linked_list_t* list, linked_list_t* target){
 		// First let's copy the first item
 		node_t *newNode = malloc(sizeof (node_t)); // this will hold temporary nodes
 		node_t *node;				   // this will hold nodes to the new linked list
-		node_t *oldNode = list->first;		   // this will hold nodes from the old linked list
+		node_t *oldNode = FIRST;		   // this will hold nodes from the old linked list
 		int i;					   // counter for loop
 		target->first = newNode;
 		node = target->first;
-		node->value = oldNode->value;
+		NODE_VAL = oldNode->value;
 		for(i = 1; i<list->count && oldNode->next != NULL; i++){
 			oldNode = oldNode->next;
 			newNode = malloc(sizeof (node_t));
-			node->next = newNode;
-			node = node->next;
-			node->value = oldNode->value;
+			NEXT = newNode;
+			node = NEXT;
+			NODE_VAL = oldNode->value;
 		}
 		target->last = node;
 		target->last->next = NULL; // make sure that last doesn't point anywhere
-		
+
 		// copy the count, of course
 		target->count = list->count;
 	}
 }
 
 void emptyLL(linked_list_t* list){
-	node_t *node = list->first;
-	node_t *prevNode = list->first;
+	node_t *node = FIRST;
+	node_t *prevNode = FIRST;
 	while(node != NULL){
 		prevNode = node;
-		node = node->next;
+		node = NEXT;
 		free(prevNode);
 	}
 	free(node); // free the last node
 	list->count = 0;
-	list->first = NULL;
-	list->last = NULL;
+	FIRST = NULL;
+	LAST = NULL;
 }
